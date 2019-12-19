eec3b39b828773bfaf31bc408e994a3e978c7221
Rongxiao Fu <2109685F@student.gla.ac.uk>
2019-12-19 14:48:41 +0000

    lift -> rise

---
+++ src/main/rise/arithmetic/simplifier/ExprSimplifier.scala
@@ -0,0 +1,34 @@
+package rise
+package arithmetic
+package simplifier
+
+import scala.language.implicitConversions
+
+/**
+ * Generic expression simplifier.
+ */
+object ExprSimplifier {
+
+  /**
+   * Simplify an expression.
+   * @param expr The expression to simplify.
+   * @return A simplified expression equivalent to expr or expr itself if it is already simplified.
+   */
+  def apply(expr: ArithExpr): ArithExpr with SimplifiedExpr = expr match {
+    case e: SimplifiedExpr => e
+    case v: Var => SimplifyVar(v)
+    case Pow(x, y) => SimplifyPow(x, y)
+    case Prod(factors) => factors.reduce(_*_)
+    case s: Sum => s.terms.reduce(_+_)
+    case Mod(a, b) => SimplifyMod(a, b)
+    case IntDiv(a, b) => SimplifyIntDiv(a, b)
+    case IfThenElse(test, t, el) => SimplifyIfThenElse(test, t, el)
+    case AbsFunction(ae) => SimplifyAbs(ae)
+    case FloorFunction(ae) => SimplifyFloor(ae)
+    case CeilingFunction(ae) => SimplifyCeiling(ae)
+    case bs:BigSum => SimplifyBigSum(bs)
+  }
+
+  def apply(aes: Seq[ArithExpr]): Seq[ArithExpr with SimplifiedExpr] = aes.map(apply)
+  def apply(aes: List[ArithExpr]): List[ArithExpr with SimplifiedExpr] = aes.map(apply)
+}
---
+++ src/main/rise/arithmetic/simplifier/SimplifyAbs.scala
@@ -0,0 +1,18 @@
+package rise
+package arithmetic
+package simplifier
+
+object SimplifyAbs {
+
+  def apply(ae: ArithExpr with SimplifiedExpr): ArithExpr with SimplifiedExpr = {
+    if (PerformSimplification())
+      ae.sign match {
+        case Sign.Positive => ae
+        case Sign.Negative => -1 * ae
+        case Sign.Unknown => new AbsFunction(ae) with SimplifiedExpr
+      }
+    else
+      new AbsFunction(ae) with SimplifiedExpr
+  }
+
+}
